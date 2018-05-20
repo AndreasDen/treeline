50d622ed42061fd2e9eebd41e4983f4f9ed29c06
TRVMP <andidennerlein@gmail.com>
2018-05-20 14:30:08 +0200

    Future self, please forgive me and don't hit me with the baseball bat again!

--- src/js/main.js
+++ src/js/main.js
@@ -1,149 +1,156 @@
-// Get the video
-var videoWrapper = $(".video-wrapper");
-var video = $(".myVideo").get(0);
-
-//Get region one
-var regionONE = $(".region-one");
-
-// Get the button
-var btn = $(".play-button span");
-var close = $(videoWrapper).find('.video-close');
-
-
-$(btn).on('click', function () {
-    playVideo();
-    $(videoWrapper).addClass('visible');
-    $(regionONE).addClass('overlay');
-});
-$(close).on('click', function () {
-    $(regionONE).addClass('overlay-fade-out');
-    setTimeout(function () {
-        $(regionONE).removeClass('overlay');
-        $(regionONE).removeClass('overlay-fade-out');
-        $(videoWrapper).removeClass('visible');
-    }, 350);
-
-    video.pause();
-});
-$(video).on('click', function () {
-    playAndPause();
-});
-
-function playVideo() {
-    video.play();
-}
-
-// Pause and play the video, and change the button text
-function playAndPause() {
-
-    if (video.paused) {
-        video.play();
-        console.log("PLAY")
-        // $(btn).removeClass("glyphicon-play");
-        // $(btn).addClass("glyphicon-pause");
+(function () {
+    "use strict";
+    if (typeof jQuery === 'undefined') {
+        window.addEventListener('load', onLoad);
     } else {
-        video.pause();
-        // $(btn).removeClass("glyphicon-pause");
-        // $(btn).addClass("glyphicon-play");
+        onLoad();
+    }
+
+    function onLoad() {
+        allVideoFunctions();
+        elementsAnimation();
+        scrollSpeed();
+        triggerModal();
+    }
+}());
+
+function allVideoFunctions() {
+    // Get the container
+    var container = $("#main");
+    // Get the video wrapper
+    var videoWrapper = $(".video-wrapper");
+    // Get the video
+    var video = $(".myVideo").get(0);
+
+    //show video
+    showVideo();
+    //hide video
+    hideVideo();
+
+    $(video).on('click', function () {
+        playAndPause();
+    });
+
+
+    // Pause and play the video
+    function playAndPause() {
+        if (video.paused) {
+            video.play();
+        } else {
+            video.pause();
+        }
+    }
+
+    function showVideo() {
+        // Get the button
+        var btn = $(".play-button span");
+        // play, show Video and set overlay on click
+        $(btn).on('click', function () {
+            video.play();
+            $(videoWrapper).addClass('visible');
+            $(container).addClass('overlay');
+        });
+    }
+
+    function hideVideo() {
+        //Get the close cta
+        var close = $(videoWrapper).find('.video-close');
+        // pause, hide Video and remove overlay on click
+        $(close).on('click', function () {
+            $(container).addClass('overlay-fade-out');
+            setTimeout(function () {
+                $(container).removeClass('overlay overlay-fade-out');
+                $(videoWrapper).removeClass('visible');
+            }, 350);
+            video.pause();
+        });
     }
 }
-
-
-// var flyingTextOffset = $('#content').find(".region-two");
-//
-// var offset;
-// var windowHeight = $(window).height();
-//
-// $(window).on("scroll", function () {
-//     offset = $(window).scrollTop() - flyingTextOffset.offset().top;
-//     console.log(offset);
-//     console.log(windowHeight);
-//     if (offset >= -windowHeight && offset <= 0) {
-//         // section entering the viewport)
-//         $(flyingTextOffset).css({"scale": "1", "opacity": "1"});
-//         $(flyingTextOffset).css("translateY", '"' + offset * 100 / windowHeight + '"');
-//     } else if (offset > 0 && offset <= windowHeight) {
-//         // console.log("SECOND")
-//         // //section leaving the viewport
-//         // scale = (1 - ( offset * 0.3 / windowHeight));
-//         // opacity = ( 1 - ( offset / windowHeight) );
-//         // translateY = 0;
-//         // boxShadowBlur = 40 * (offset / windowHeight);
-//     }
-// });
-
-
-var $animation_elements = $('.animation-element');
-var $fixed_elements = $('.fixed-element');
-var $fixed_element_offset = $fixed_elements.offset().top;
-var $window = $(window);
-
-
-function check_if_in_view() {
-    var window_height = $window.height();
-    var window_top_position = $window.scrollTop();
-    var window_bottom_position = (window_top_position + window_height);
-
-
-
-    $.each($animation_elements, function () {
-        var $element = $(this);
-        var element_height = $element.outerHeight();
-        var element_top_position = $element.offset().top;
-        var element_bottom_position = (element_top_position + element_height);
-
-        //check to see if this current container is within viewport
-        if ((element_bottom_position >= window_top_position) &&
-            (element_top_position < window_bottom_position - 100)) {
-            $element.addClass('in-view');
-        } else {
-            $element.removeClass('in-view');
-        }
-    });
-
-    $.each($fixed_elements, function() {
-        var $element = $(this);
-
-        console.log( $fixed_element_offset)
-        //check to see if this current container is within viewport
-        if (window.pageYOffset >= $fixed_element_offset -30) {
-            $element.addClass('fixed');
-        }
-        else {
-            $element.removeClass('fixed');
-        }
-    });
-}
-
-$window.on('scroll resize', check_if_in_view);
-$window.trigger('scroll');
-
-$.fn.moveIt = function(){
+function elementsAnimation() {
+    var $animation_elements = $('.animation-element');
+    var $fixed_elements = $('.fixed-element');
+    var $fixed_element_offset = $fixed_elements.offset().top;
     var $window = $(window);
-    var instances = [];
 
-    $(this).each(function(){
-        instances.push(new moveItItem($(this)));
-    });
+    $window.on('scroll resize', check_if_in_view);
+    $window.trigger('scroll');
 
-    window.addEventListener('scroll', function(){
-        var scrollTop = $window.scrollTop();
-        instances.forEach(function(inst){
-            inst.update(scrollTop);
+    function check_if_in_view() {
+        var window_height = $window.height();
+        var window_top_position = $window.scrollTop();
+        var window_bottom_position = (window_top_position + window_height);
+
+        //set class in-view on elements
+        $.each($animation_elements, function () {
+            var $element = $(this);
+            var element_height = $element.outerHeight();
+            var element_top_position = $element.offset().top;
+            var element_bottom_position = (element_top_position + element_height);
+
+            //check to see if this current container is within viewport
+            if ((element_bottom_position >= window_top_position) &&
+                (element_top_position < window_bottom_position - 100)) {
+                $element.addClass('in-view');
+            } else {
+                $element.removeClass('in-view');
+            }
         });
-    }, {passive: true});
+
+        //set class fixed on elements
+        $.each($fixed_elements, function () {
+            var $element = $(this);
+
+            //check to see if this current container is within viewport
+            if (window.pageYOffset >= $fixed_element_offset - 30) {
+                $element.addClass('fixed');
+            }
+            else {
+                $element.removeClass('fixed');
+            }
+        });
+    }
 }
+function scrollSpeed() {
 
-var moveItItem = function(el){
-    this.el = $(el);
-    this.speed = parseInt(this.el.attr('data-scroll-speed'));
-};
+    $.fn.moveIt = function () {
+        var $window = $(window);
+        var instances = [];
 
-moveItItem.prototype.update = function(scrollTop){
-    this.el.css('transform', 'translateY(' + -(scrollTop / this.speed) + 'px)');
-};
+        $(this).each(function () {
+            instances.push(new moveItItem($(this)));
+        });
+
+        window.addEventListener('scroll', function () {
+            var scrollTop = $window.scrollTop();
+            instances.forEach(function (inst) {
+                inst.update(scrollTop);
+            });
+        }, {passive: true});
+    }
+
+    var moveItItem = function (el) {
+        this.el = $(el);
+        this.speed = parseInt(this.el.attr('data-scroll-speed'));
+    };
+
+    moveItItem.prototype.update = function (scrollTop) {
+        this.el.css('transform', 'translate(-50%,' + -(scrollTop / this.speed) + 'px)');
+    };
 
 // Initialization
-$(function(){
-    $('[data-scroll-speed]').moveIt();
-});
+    $(function () {
+        $('[data-scroll-speed]').moveIt();
+    });
+}
+function triggerModal() {
+    $('#myModal').on('shown.bs.modal', function () {
+        $('#myInput').trigger('focus')
+    })
+}
+
+
+
+
+
+
+
